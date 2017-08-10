a091fcdb1178d49ced71409640160f224a9d7993
github username <github email address>
2017-08-10 09:09:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// Create a constructor function for each of the following:
+
+// + Ferret with `name`, `age`, and `foodRequirements` properties
+// + Bird with `name` and `breed` properties
+// + Fish with `name`, `species`, and `waterConditions` properties
+
+
+function Dog(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+function Cat(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+
+
+// function Sandwich(bread, crust, meat, condiments, veggies, cheese) {
+//   this.breadType = bread;
+//   this.crust = crust;
+//   this.meat = meat;
+//   this.condiments = condiments;
+//   this.veggies = veggies;
+//   this.cheese = cheese;
+// }
